5149443548d14c28be4c6500111125d1ffbdea25
dalebert <dalebert11@gmail.com>
2024-01-18 17:21:56 -0500

    Completed Building A React App 3

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App.js';
 import './index.css';
-import CardList from './CardList.js'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots.js';
 
-ReactDOM.render(
-    <CardList robots={robots} />
-  , document.getElementById('root')
-);
+const root = document.getElementById('root');
+createRoot(root).render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
